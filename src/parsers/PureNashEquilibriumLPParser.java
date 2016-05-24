40868488448c0e505a5ac1a69b181feed881be21
Niels Elgers <Niels Elgers>
2016-05-25 00:25:21 +0200

    Some comments

--- src/parsers/PureNashEquilibriumLPParser.java
+++ src/parsers/PureNashEquilibriumLPParser.java
@@ -16,118 +16,149 @@ public class PureNashEquilibriumLPParser {
 	 * @param name The name of the .lp file.
 	 * @throws FileNotFoundException
 	 * @post The .lp file will be located at: path + name + ".lp" and will have a UTF-8 encoding.
+	 * http://www.hindawi.com/journals/mpe/2014/640960/
 	 */
 	
-	public static void gameToPNELPfile(Game game, String path, String name) throws FileNotFoundException {
+	public static void gameToPNELPfile(Game game, String path, String name) {
 		
 		try {
-			PrintWriter writer = new PrintWriter(path + name + ".lp", "UTF-8"); //Create output stream.
+			PrintWriter toFile = new PrintWriter(path + name + ".lp", "UTF-8"); //Create output stream.
 
 			double beta = computeBeta(game) + 100; // +100 just to be sure.
 			
-			writer.println("Subject To");
+			//Objective function.
+			//We don't actually need one since we're looking for a feasible solution only.
+			//A 'dummy' objective function is added, regardless.
+			toFile.println("Maximize");
+			toFile.println(" 0");
+			
+			//The constraints.
+			toFile.println("Subject To");
 			for (int player = 0; player < game.getNumPlayers(); player++) {
 				OutcomeIterator outcomesExcept = new OutcomeIterator(game, player);
 				
-				//Expected payoff constraints (EPC).
+				//Payoff constraints (first constraint in LP from paper).
+				//Forces that no player can improve his payoff by deviating.
 				for (int action = 1; action <= game.getNumActions(player); action++) {
 					outcomesExcept.reset();
-					writer.print(" epc_" + player + "/" + action + ": ");
+					//Constraint name.
+					toFile.print(" epc_" + player + "!" + action + ": ");
+					
+					//Summation part.
 					while (outcomesExcept.hasNext()) {
 						int[] outcome = outcomesExcept.next().clone(); outcome[player] = action;
 						
 						double payoff = game.getPayoff(outcome, player);
 						
-						if (outcomesExcept.getCount() != 0 && payoff >= 0) writer.print("+");
+						if (outcomesExcept.getCount() != 0 && payoff >= 0) toFile.print("+");
 						
-						writer.print(payoff + " " + makeY(game, outcome, player) + " ");
-				
+						toFile.print(payoff + " " + makeY(game, outcome, player) + " ");
 					}
 					
-					writer.println("+ lambda_" + player + "/" + action + " - mu_" + player + " = 0");
+					//Lambda, mu part.
+					toFile.println("+ lambda_" + player + "!" + action + " - mu_" + player + " = 0");
 				}
 				
 				
-				//distribution constraint (DC).
-				writer.print(" dc_" + player + ": ");
+				//Distribution constraint (second constraint).
+				//Forces a strategy to be a probability distribution.
+				
+				//Constraint name.
+				toFile.print(" dc_" + player + ": ");
+				
+				//Sum of all probabilities of player...
 				for (int action = 1; action <= game.getNumActions(player); action++) {
-					if (action != 1) writer.print(" + ");
-					writer.print("x_" + player + "/" + action);
+					if (action != 1) toFile.print(" + ");
+					toFile.print("x_" + player + "!" + action);
 				}
-				writer.println(" = 1");
+				//... must equal one.
+				toFile.println(" = 1");
 				
-				//beta constraint (BC).
+				//Beta constraint (third constraint).
+				//Forces lambda to be zero for the action of a player in equilibrium.
+				//Otherwise lambda must be smaller than beta.
+				//beta is bigger or equal than the maximum payoff gain of all players.
+				//This constraint forces mu (first constraint) to be equal to the payoff value of a player in equilibrium.
 				for (int action = 1; action <= game.getNumActions(player); action++) {
-					writer.print(" bc_" + player + "/" + action + ": ");
-					writer.print("lambda_" + player + "/" + action + " ");
-					
-					if (beta >= 0) writer.print("+");
-					
-					writer.println(beta + " x_" + player + "/" + action + " <= " + beta);
+					//Constraint name.
+					toFile.print(" bc_" + player + "!" + action + ": ");
+					//The constraint.
+					toFile.print("lambda_" + player + "!" + action + " ");
+					if (beta >= 0) toFile.print("+");
+					toFile.println(beta + " x_" + player + "!" + action + " <= " + beta);
 				}
 				
+				//Kinda hazy on what these constraints force.
+				//Constraints involving y variable.
 				outcomesExcept.reset();
 				while (outcomesExcept.hasNext()) {
 					int[] outcome = outcomesExcept.next().clone();
 					
-					//sum constraint (SC).
-					writer.print(" sum_" + player + "/" + outcomesExcept.getCount() + ": ");
-					writer.print(makeY(game, outcome, player));
+					toFile.print(" sum_" + player + "!" + outcomesExcept.getCount() + ": ");
+					toFile.print(makeY(game, outcome, player));
 					
 					for (int playerH = 0; playerH < game.getNumPlayers(); playerH++) {
 						if (playerH == player) continue;;
 						
-						writer.print(" - x_" + playerH + "/" + outcome[playerH]);
+						toFile.print(" - x_" + playerH + "!" + outcome[playerH]);
 					}
 					
 					
-					writer.println(" >= " + -(game.getNumPlayers() - 2));
+					toFile.println(" >= " + -(game.getNumPlayers() - 2));
 					
-					//I don't know constraint (IDK)
 					for (int playerK = 0; playerK < game.getNumPlayers(); playerK++) {
 						if (playerK == player) continue;
 						
-						writer.print(" idk_" + player + "/" + outcomesExcept.getCount() + "/" + playerK + ": ");
-						writer.print(makeY(game, outcome, player));
-						writer.println(" - x_" + playerK + "/" + outcome[playerK] + " <= 0");	
+						toFile.print(" idk_" + player + "!" + outcomesExcept.getCount() + "!" + playerK + ": ");
+						toFile.print(makeY(game, outcome, player));
+						toFile.println(" - x_" + playerK + "!" + outcome[playerK] + " <= 0");	
 					}
 				}
 				
 				
 			}
 			
-			writer.println("Bounds");
+			//Bounds.
+			//µ variables are free.
+			//Other variables positive (which is the case by default).
+			toFile.println("Bounds");
 			for (int player = 0; player < game.getNumPlayers(); player++)
-				writer.println(" mu_" + player + " free");
+				toFile.println(" mu_" + player + " free");
 			
-			writer.println("Binary");
+			//Binaries.
+			//Actions are binary: they're either played or not (pure strategy).
+			toFile.println("Binary");
 			for (int player = 0; player < game.getNumPlayers(); player++)
 				for (int action = 1; action <= game.getNumActions(player); action++) 
-					writer.print(" x_" + player + "/" + action);
+					toFile.print(" x_" + player + "!" + action);
 			
-			writer.println();
-			writer.println("End");
-			writer.close();
+			toFile.println();
+			toFile.println("End");
+			toFile.close();
 			
 			System.out.println(game.getName() + " PNE LP was succesfully written to " + path + name + ".lp");
 			
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
 	
+	//Create y variable name.
 	private static String makeY(Game game, int[] outcome, int exceptPlayer) {
 		String y = "y";
 		
 		for (int player = 0; player < game.getNumPlayers(); player++) {
 			if (player == exceptPlayer) continue;
 			
-			y += "_" + player + "/" + outcome[player];
+			y += "_" + player + "!" + outcome[player];
 		}
 		
 		return y;
 	}
 	
+	//Compute beta variable.
 	private static double computeBeta(Game game) {
 		OutcomeIterator outcomeIterator = new OutcomeIterator(game);
 		
